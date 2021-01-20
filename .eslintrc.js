8f81032e9ea8b66c3719580909ee736f7cac3cc1
chnk <spica30@gmail.com>
2021-01-20 11:06:55 +0900

    Prettierのコードフォーマット適用

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,56 +1,54 @@
 module.exports = {
-    "env": {
-        "browser": true
-    },
-    "parser": "babel-eslint",
-    "globals": {
-        "$": true,
-        "google": true
-    },
-    "extends": [
-        "airbnb-base",
-        "plugin:prettier/recommended"
+  env: {
+    browser: true,
+  },
+  parser: 'babel-eslint',
+  globals: {
+    $: true,
+    google: true,
+  },
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  rules: {
+    'comma-dangle': 0,
+    'no-param-reassign': 0,
+    camelcase: 0,
+    'func-names': 0,
+    'no-underscore-dangle': 0,
+    'no-use-before-define': 0,
+    'no-new': 0,
+    'consistent-return': 0,
+    'comma-spacing': [
+      2,
+      {
+        before: false,
+        after: true,
+      },
     ],
-    "rules": {
-        "comma-dangle": 0,
-        "no-param-reassign": 0,
-        "camelcase": 0,
-        "func-names": 0,
-        "no-underscore-dangle": 0,
-        "no-use-before-define": 0,
-        "no-new": 0,
-        "consistent-return": 0,
-        "comma-spacing": [
-            2, {
-                "before": false,
-                "after": true
-            }
-        ],
-        "import/extensions": [
-            "error",
-            "always",
-            {
-                "js": "never",
-                "jsx": "never",
-                "ts": "never",
-                "tsx": "never"
-            }
-        ],
-        "prettier/prettier": [
-            "error",
-            {
-                "bracketSpacing": true,
-                "printWidth": 100,
-                "singleQuote": true,
-                "tabWidth": 2
-            }
-        ],
-        "lines-between-class-members": [
-            "error",
-            "always",
-            {
-                exceptAfterSingleLine: true
-            }
-        ]
-    }
-}
+    'import/extensions': [
+      'error',
+      'always',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        bracketSpacing: true,
+        printWidth: 100,
+        singleQuote: true,
+        tabWidth: 2,
+      },
+    ],
+    'lines-between-class-members': [
+      'error',
+      'always',
+      {
+        exceptAfterSingleLine: true,
+      },
+    ],
+  },
+};
